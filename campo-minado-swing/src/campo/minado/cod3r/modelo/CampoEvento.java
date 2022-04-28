07b4f4fcbd850155a84f46129deeb14461190729
Pedro Ribeiro <ribeiro.tpedro@gmail.com>
2022-04-28 13:17:09 -0300

    delete files

--- campo-minado-swing/src/campo/minado/cod3r/modelo/CampoEvento.java
+++
@@ -1,6 +0,0 @@
-package campo.minado.cod3r.modelo;
-
-public enum CampoEvento {
-
-	ABRIR, MARCAR, DESMARCAR, EXPLODIR, REINICIAR
-}
--- campo-minado-swing/src/campo/minado/cod3r/modelo/CampoObservador.java
+++
@@ -1,7 +0,0 @@
-package campo.minado.cod3r.modelo;
-
-@FunctionalInterface
-public interface CampoObservador {
-
-	public void eventoOcorreu(Campo campo, CampoEvento evento);
-}
